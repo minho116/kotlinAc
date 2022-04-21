0390df06f3fff085ef5338dcf9909a92d759a3f5
minho Kang <62061644+minho116@users.noreply.github.com>
2022-04-21 12:44:36 +0900

    Update MainActivity.kt

--- mango_contents/app/src/main/java/com/domino/mango_contents/MainActivity.kt
+++ mango_contents/app/src/main/java/com/domino/mango_contents/MainActivity.kt
@@ -26,9 +26,35 @@ class MainActivity : AppCompatActivity() {
 
         items.add(
             ContentsModel("https://www.mangoplate.com/restaurants/Cx9Cg9oPDTpK",
-            "https://mp-seoul-image-production-s3.mangoplate.com/435193/533108_1648017093717_22054?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-            "팔레드신")
+                "https://mp-seoul-image-production-s3.mangoplate.com/435193/533108_1648017093717_22054?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "팔레드신")
         )
+        items.add(
+            ContentsModel("https://www.mangoplate.com/restaurants/2J0axpUplhy7",
+                "https://mp-seoul-image-production-s3.mangoplate.com/283112/53774_1597724351125_60274?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "옥동식")
+        )
+        items.add(
+            ContentsModel("https://www.mangoplate.com/restaurants/lx7BAVdqCpLm",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1601255_1629723658048924.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "아스트랄")
+        )
+        items.add(
+            ContentsModel("https://www.mangoplate.com/restaurants/Lj8LbMuvciWk",
+                "https://mp-seoul-image-production-s3.mangoplate.com/872233_1620809693230837.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "울다이닝")
+        )
+        items.add(
+            ContentsModel("https://www.mangoplate.com/restaurants/ne_LkgB6a_Fp",
+                "https://mp-seoul-image-production-s3.mangoplate.com/463989_1639533259805174.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "비아 톨레도 파스타바")
+        )
+        items.add(
+            ContentsModel("https://www.mangoplate.com/restaurants/c0QnYuxR7ioD",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1454700_1615468261313272.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "심퍼티쿠시")
+        
+       
 
 
 
